18d35ba5f88ded98bc6685e22ff48269ffe925a2
yasser <yasser.laiss01@gmail.com>
2025-05-07 19:54:01 +0100

    setup dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-    return <div> </div>;
+    return (
+        <div>
+            <h1>hello world</h1>
+            <h6>goodby world</h6>
+            <p style={{ color: "var(--color-violet)" }}>testing</p>
+            <button>click me</button>
+        </div>
+    );
 }
 
 export default App;
